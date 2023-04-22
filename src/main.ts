2b67ecc5b1efa56684fbc20dfc23de919ea65007
ajnaskp <ajnaskp67@gmail.com>
2023-04-22 14:42:30 +0530

    ff

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({ origin: 'http://localhost:4200', credentials: true });
+  app.enableCors({ origin: 'https://easylearn.store', credentials: true });
 
   await app.listen(3000);
 }
 bootstrap();
+https://easylearn.store
