e2217651eb113d7a59ca916035e24d2f09cfbd0d
ajnaskp <ajnaskp67@gmail.com>
2023-04-23 08:55:23 +0530

    cors origin change

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({ origin: 'https://easylearn.store', credentials: true });
+  app.enableCors({ origin: 'https://api.easylearn.store', credentials: true });
 
   await app.listen(3000);
 }
